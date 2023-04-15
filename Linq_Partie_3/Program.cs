3f2cdb3a7be1d6892c2d22bb5169aa40c0c16e91
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-04-15 21:20:28 +0000

    Non Query syntax

---
+++ Linq_Partie_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Partie_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Names = new List<string>()
+           {
+               "Red","BOOK","Car","Computer","Mobile","Program","Driver","Window","Key","Home","Remote","Cami"
+           };
+
+            //Non Query syntax   &&  lambda expression
+            
+
+            var Result =  Names.Where(item => item.Contains("C")).Take(2);
+
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
