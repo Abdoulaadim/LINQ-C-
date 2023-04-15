bab6779602a1edd79e97e2aa0be2c6bdce9f1490
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-04-15 21:35:28 +0000

    Query syntax

---
+++ Linq_Partie_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Partie_4
+{
+    internal class Program
+    {
+        class Book
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+
+            public string Author { get; set; }
+        }
+        static void Main(string[] args)
+        {
+                Book[] books = { new Book()   { Id = 6 , Author ="Abdoulaadim" , Title = "C#" },
+                                 new Book()   { Id = 7 , Author ="Yahya" , Title = "Java" },
+                                 new Book()   { Id = 4 , Author ="jabrane" , Title = "Dotnet" }
+                               };
+
+                // Query syntax
+
+                var list  = from item
+                            in books 
+                            orderby item.Id descending
+                            select item;
+                
+            foreach (Book item in list)
+            {
+                Console.WriteLine("Id= {0} , Title = {1}, Author= {2}",item.Id,item.Title,item.Author);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
