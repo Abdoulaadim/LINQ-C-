35b34ee5e0cb259fbf475864a61fd3f30b0ec580
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-04-15 21:07:07 +0000

    Query syntax

---
+++ Linq_Partie_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Partie_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Names = new List<string>()
+           {
+               "Red","BOOK","Car","Computer","Mobile","Program","Driver","Window","Key","Home"
+           };
+
+            //Query Syntax
+
+            var result  =  from item in Names select item;
+
+            foreach (var items in result)
+            {
+                Console.WriteLine(items);
+            }
+            Console.ReadLine();
+        }
+    }
+}
