5573d369ba777571fd259bd0e7e5b4aee6a03256
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-04-15 21:40:24 +0000

    fluent syntax

--- Linq_Partie_4/Program.cs
+++ Linq_Partie_4/Program.cs
@@ -28,11 +28,26 @@ namespace Linq_Partie_4
                             in books 
                             orderby item.Id descending
                             select item;
-                
+
             foreach (Book item in list)
             {
-                Console.WriteLine("Id= {0} , Title = {1}, Author= {2}",item.Id,item.Title,item.Author);
+                Console.WriteLine("Id= {0} , Title = {1}, Author= {2}", item.Id, item.Title, item.Author);
             }
+            Console.WriteLine();
+            Console.WriteLine("_________________________Non query __________________________________________");
+            Console.WriteLine();
+            //Non query Syntax  / fluent syntax 
+
+
+            var items  = books.OrderByDescending(item => item.Id);
+
+
+            foreach (Book item in items)
+            {
+                Console.WriteLine("Id= {0} , Title = {1}, Author= {2}", item.Id, item.Title, item.Author);
+            }
+
+
 
             Console.ReadLine();
         }
