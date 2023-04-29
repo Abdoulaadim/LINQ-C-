8a3858bf84903a4ea3a6df2eeb32a2c805d8edfb
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-04-29 22:18:22 +0100

    creation des classe

---
+++ Linq_Partie_.7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Partie_._7
+{
+    internal class Program
+    {
+        class Teacher
+        {
+            public string Name { get; set; }
+
+        }
+        class Student
+        {
+            public string Name { get; set; }
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
