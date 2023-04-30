a57f88dfbf2fb4afe8099eb19c1e0bc29c215a2d
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-04-30 23:45:09 +0100

    concat group

---
+++ Linq_Partie_8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Partie_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] group1 = { "Abdoulaadim", "Hamid", "Laila", "Kabira" };
+            string[] group2 = { "Hind", "Abdoulaadim", "Kabira", "Yassir" };
+
+            
+        }
+    }
+}
