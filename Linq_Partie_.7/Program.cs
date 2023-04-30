0227d6593730f616f909e4a8e3ea7b47a8ac2733
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-04-30 17:56:48 +0100

    remplissage

--- Linq_Partie_.7/Program.cs
+++ Linq_Partie_.7/Program.cs
@@ -20,6 +20,28 @@ namespace Linq_Partie_._7
         }
         static void Main(string[] args)
         {
+            Teacher[] teachers =
+            {
+                new Teacher { Name = "Abdoulaadim"},
+                new Teacher { Name = "Ahmed"},
+                new Teacher { Name = "Yahya"},
+                new Teacher { Name = "Abdelaalim"},
+                new Teacher { Name = "Niimatallah"},
+                new Teacher { Name = "Hind"},
+                new Teacher { Name = "Kabira"}
+
+            };
+
+            Student[] students =
+            {
+                new Student { Name ="Laila"},
+                new Student { Name ="Morad"},
+                new Student { Name ="Reda"},
+                new Student { Name ="Imane"},
+                new Student { Name ="Ghita"},
+                new Student { Name ="Ikram"},
+                new Student { Name ="Yassmine"},
+            };
         }
     }
 }
