5716700082f64493db8064b65838d00629182837
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-04-30 23:50:40 +0100

    concat && Distinct && Union

--- Linq_Partie_8/Program.cs
+++ Linq_Partie_8/Program.cs
@@ -13,7 +13,43 @@ namespace Linq_Partie_8
             string[] group1 = { "Abdoulaadim", "Hamid", "Laila", "Kabira" };
             string[] group2 = { "Hind", "Abdoulaadim", "Kabira", "Yassir" };
 
-            
+
+            //concat
+            var concatGroup = group1.Concat(group2);
+
+            foreach (var group in concatGroup)
+            {
+                Console.WriteLine(group);
+            }
+
+            Console.WriteLine("//////////////////////////////");
+            Console.WriteLine();
+
+            // Distinct 
+            var concatGroup1 = group1.Concat(group2).Distinct();
+
+            foreach (var group in concatGroup1)
+            {
+                Console.WriteLine(group);
+            }
+
+            Console.WriteLine("//////////////////////////////");
+            Console.WriteLine();
+
+            // Union 
+            var concatGroup2 = group1.Union(group2);
+
+            foreach (var group in concatGroup2)
+            {
+                Console.WriteLine(group);
+            }
+
+            Console.ReadKey();
+
+            Console.ReadKey();
+
+
+
         }
     }
 }
