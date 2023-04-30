1dfd5984e4cc2d5ba17325ed35271e5053fca579
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-04-30 18:11:11 +0100

    Query syntax result

--- Linq_Partie_.7/Program.cs
+++ Linq_Partie_.7/Program.cs
@@ -22,25 +22,25 @@ namespace Linq_Partie_._7
         {
             Teacher[] teachers =
             {
-                new Teacher { Name = "Abdoulaadim"},
-                new Teacher { Name = "Ahmed"},
-                new Teacher { Name = "Yahya"},
-                new Teacher { Name = "Abdelaalim"},
-                new Teacher { Name = "Niimatallah"},
-                new Teacher { Name = "Hind"},
-                new Teacher { Name = "Kabira"}
+                new Teacher() { Name = "Abdoulaadim"},
+                new Teacher() { Name = "Ahmed"},
+                new Teacher() { Name = "Yahya"},
+                new Teacher() { Name = "Abdelaalim"},
+                new Teacher() { Name = "Niimatallah"},
+                new Teacher() { Name = "Hind"},
+                new Teacher() { Name = "Kabira"}
 
             };
 
             Student[] students =
             {
-                new Student { Name ="Laila"},
-                new Student { Name ="Morad"},
-                new Student { Name ="Reda"},
-                new Student { Name ="Imane"},
-                new Student { Name ="Ghita"},
-                new Student { Name ="Ikram"},
-                new Student { Name ="Yassmine"},
+                new Student() { Name ="Abdoulaadim"},
+                new Student() { Name ="Yahya"},
+                new Student() { Name ="Reda"},
+                new Student() { Name ="Imane"},
+                new Student() { Name ="Ghita"},
+                new Student() { Name ="Ikram"},
+                new Student() { Name ="Yassmine"}
             };
 
             //query Syntax
@@ -48,7 +48,14 @@ namespace Linq_Partie_._7
             var JoinList = from teacher in teachers
                            join student in students
                            on teacher.Name equals student.Name
-                           select teacher; 
+                           select teacher;
+
+            Console.WriteLine("---------------");
+            foreach (var element in JoinList)
+            {
+                Console.WriteLine("Name = " + element.Name);
+            }
+            Console.ReadKey();
         }
     }
 }
