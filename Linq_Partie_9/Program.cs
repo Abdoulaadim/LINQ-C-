5ed2a6e7b644982321de78c5ec9f2b05b4cad464
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-05-01 19:38:04 +0100

    lastElement

--- Linq_Partie_9/Program.cs
+++ Linq_Partie_9/Program.cs
@@ -13,10 +13,23 @@ namespace Linq_Partie_9
             string[] names = { "Abdoulaadim", "Youssef", "Rachid", "Kabira", "Hind", "Laila" };
 
             //var firstElement = names.First();
-            //var firstElement = names.First(a => a.StartsWith("H"));
-            var firstElement = names.FirstOrDefault(a => a.StartsWith("T")); // return null
+            var firstElement = names.First(a => a.StartsWith("H"));
+            //var firstElement = names.FirstOrDefault(a => a.StartsWith("T")); // return null
 
+            Console.WriteLine("////////////////firstElement///////////////////");
+            Console.WriteLine();
             Console.WriteLine(firstElement);
+            Console.WriteLine();
+
+
+            var lastElement = names.Last(); // return null
+
+
+            Console.WriteLine("////////////////LastElement///////////////////");
+            Console.WriteLine();
+            Console.WriteLine(lastElement);
+            Console.WriteLine();
+
 
             Console.ReadKey();
 
