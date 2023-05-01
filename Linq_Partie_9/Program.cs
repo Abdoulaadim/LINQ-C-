c7368b1aa3c17215ba0cb4c9f786a4e8a6a97171
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-05-01 18:53:09 +0100

    FirstOrDefault

--- Linq_Partie_9/Program.cs
+++ Linq_Partie_9/Program.cs
@@ -12,7 +12,9 @@ namespace Linq_Partie_9
         {
             string[] names = { "Abdoulaadim", "Youssef", "Rachid", "Kabira", "Hind", "Laila" };
 
-            var firstElement = names.First(a => a.StartsWith("H"));
+            //var firstElement = names.First();
+            //var firstElement = names.First(a => a.StartsWith("H"));
+            var firstElement = names.FirstOrDefault(a => a.StartsWith("T")); // return null
 
             Console.WriteLine(firstElement);
 
