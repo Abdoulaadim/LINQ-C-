0212ad05d9fc786fdb37b5eb59946f3f45a6645e
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2023-05-03 06:26:42 +0100

    table numbers

---
+++ Linq_Partie_10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Partie_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 7, 3, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+
+        }
+    }
+}
